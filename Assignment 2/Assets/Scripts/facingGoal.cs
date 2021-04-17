50fc7ff00c4c1aaf968c0aac1c0957bd6f418c91
Harrison Chebuk <31672501+hchebuk@users.noreply.github.com>
2021-04-16 20:21:42 -0700

    Added particles when facing goal
    Particles now appear when facing towards the goal

---
+++ Assignment 2/Assets/Scripts/facingGoal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facingGoal : MonoBehaviour
+{
+    public Transform other;
+    public GameObject particle;
+
+    void Update()
+    {
+        if (other)
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 toOther = other.position - transform.position;
+
+            if (Vector3.Dot(forward, toOther) > 0)
+            {
+                Instantiate(particle, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
