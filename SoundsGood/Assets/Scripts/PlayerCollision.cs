7c752e5f1874fb99919d301cb217a198ae7b7dcf
Kenny Nguyen <72421222+itsmeKenny@users.noreply.github.com>
2021-04-26 10:05:48 -0700

    Finished Base
    Two small issues, color change after restart and winning screen not triggering.

---
+++ SoundsGood/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle") 
+        {
+            //Add function when player hits obstacle
+            
+            FindObjectOfType<GameManager>().EndGame(); //Looks for gamemanager object and calls that
+        }
+    }
+}
